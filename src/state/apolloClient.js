64b8faf23d07b5320ced2202e9db39a6188eb375
Briet Sparks <bws46@cornell.edu>
2017-08-28 23:50:32 -0500

    tweak

--- src/state/apolloClient.js
+++ src/state/apolloClient.js
@@ -1,5 +1,6 @@
 import { ApolloClient, createNetworkInterface } from 'react-apollo';
 import { graphqlApi } from '../config';
+import { accessTokenKey } from '../substates/auth/constants';
 
 const networkInterface = createNetworkInterface({
   uri: graphqlApi.api.root
@@ -9,7 +10,7 @@ networkInterface.use([{
   applyMiddleware(req, next) {
     req.options.headers = req.options.headers || {};
 
-    const token = localStorage.getItem('id_token');
+    const token = localStorage.getItem(accessTokenKey);
     req.options.headers.authorization = token ? `Bearer ${token}` : null;
 
     console.log(req);
